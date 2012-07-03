a1eb6846fbf6883b251dc49936f443272ddbd52e
Frédéric Bertolus <fred.bertolus@gmail.com>
2012-07-03 22:59:04 +0200

    go format

--- main.go
+++ main.go
@@ -1,59 +1,56 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"html/template"
 	"bytes"
+	"fmt"
+	"github.com/fredb219/yack"
 	"github.com/fredb219/yack-server/web"
-    "github.com/fredb219/yack"
+	"html/template"
+	"net/http"
 )
 
-
-
-
 type StaticFilePath struct {
-    StaticUrl string
+	StaticUrl string
 }
 
-type IndexHandle struct{
+type IndexHandle struct {
 	indexCache string
 }
 
 func NewIndexHandle(resourcePath string) *IndexHandle {
 	var this IndexHandle
-	
+
 	t, _ := template.ParseFiles("resources/templates/index.html")
-	param := StaticFilePath{StaticUrl:resourcePath}
+	param := StaticFilePath{StaticUrl: resourcePath}
 
 	var data bytes.Buffer
-    t.Execute(&data, param)
-    this.indexCache = data.String() 
+	t.Execute(&data, param)
+	this.indexCache = data.String()
 
 	return &this
 }
 
 func (this IndexHandle) ServeHTTP(
-			w http.ResponseWriter, 
-			r *http.Request) {
-	
-	fmt.Println("IndexHandle: request="+r.URL.RequestURI());
-	fmt.Fprintf(w, this.indexCache);
+	w http.ResponseWriter,
+	r *http.Request) {
+
+	fmt.Println("IndexHandle: request=" + r.URL.RequestURI())
+	fmt.Fprintf(w, this.indexCache)
 }
-			
+
 func main() {
-	fmt.Println("Yack 1.0.0a8");
-	
-	yack.Init();
-	
+	fmt.Println("Yack 1.0.0a8")
+
+	yack.Init()
+
 	var indexHandle = NewIndexHandle("static/")
 	var loginHandle = web.NewLoginHandle()
 	var commandHandle = web.NewCommandHandle()
 	http.Handle("/", indexHandle)
 	http.Handle("/yack/login", loginHandle)
 	http.Handle("/yack/command", commandHandle)
-	http.Handle("/static/",http.StripPrefix("/static", http.FileServer(http.Dir("resources/static"))))
-	
+	http.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir("resources/static"))))
+
 	fmt.Println("Start server on :4000")
 	http.ListenAndServe("localhost:4000", nil)
 }
--- web/loginHandle.go
+++ web/loginHandle.go
@@ -1,17 +1,16 @@
 package web
 
 import (
-	"fmt"
-	"io/ioutil"
 	"encoding/json"
-	"net/http"
-	"strconv"
-	"net/url"
+	"fmt"
 	"github.com/fredb219/yack"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strconv"
 )
 
-
-type LoginHandle struct{
+type LoginHandle struct {
 	indexCache string
 }
 
@@ -22,105 +21,101 @@ func NewLoginHandle() *LoginHandle {
 }
 
 type loginSucessMessage struct {
-	Status bool  `json:"status"`
-	Id int  `json:"id"`
-	Name string  `json:"name"`
-	Token string  `json:"token"`
+	Status bool   `json:"status"`
+	Id     int    `json:"id"`
+	Name   string `json:"name"`
+	Token  string `json:"token"`
 }
-
 
 type loginFailMessage struct {
-	Status bool `json:"status"`
-	Error string `json:"error"`
+	Status bool   `json:"status"`
+	Error  string `json:"error"`
 }
 
-
 func (this LoginHandle) ServeHTTP(
-			w http.ResponseWriter, 
-			r *http.Request) {
-	
-	fmt.Println("LoginHandle: request="+r.URL.RequestURI());
-	
-	
+	w http.ResponseWriter,
+	r *http.Request) {
+
+	fmt.Println("LoginHandle: request=" + r.URL.RequestURI())
+
 	var method = r.URL.Query().Get("method")
-	
-	
-	fmt.Println("LoginHandle: method="+method);
-	
+
+	fmt.Println("LoginHandle: method=" + method)
+
 	if method == "check" {
 		var token = r.URL.Query().Get("token")
-		var id , _= strconv.Atoi(r.URL.Query().Get("id"))
-		
+		var id, _ = strconv.Atoi(r.URL.Query().Get("id"))
+
 		// database.users.get(auth_token=token, id=id)
 		var user = yack.GetModel().Users.GetByAuthToken(token, id)
-        var data []byte
-        
-        if user == nil {
-            var m = loginFailMessage{false, fmt.Sprintf("AuthToken is invalid or perimed: %s", token)}
-        	data, _ = json.Marshal(m)
-        } else {
-		    var m = loginSucessMessage{true, user.Id(), user.DisplayName(), user.AuthToken()}
-		    data, _ = json.Marshal(m)
+		var data []byte
+
+		if user == nil {
+			var m = loginFailMessage{false, fmt.Sprintf("AuthToken is invalid or perimed: %s", token)}
+			data, _ = json.Marshal(m)
+		} else {
+			var m = loginSucessMessage{true, user.Id(), user.DisplayName(), user.AuthToken()}
+			data, _ = json.Marshal(m)
 		}
-		fmt.Println("generated verify json: ",string(data))
+		fmt.Println("generated verify json: ", string(data))
 		w.Header().Set("Content-Type", "application/javascript")
 		w.WriteHeader(200)
 		w.Write(data)
-		
-        //except ObjectDoesNotExist:
-        //    data = json.dumps([{'status':  False}])
-    
+
+		//except ObjectDoesNotExist:
+		//    data = json.dumps([{'status':  False}])
+
 	} else if method == "browserid" {
-	 	var token = r.URL.Query().Get("token")
-        
-        type browserIdVerificationResponse struct {
-        	Status string `json:"status"`
-        	Email string `json:"email"`
-        	Audience string `json:"audience"`
-        	Expires int64 `json:"expiers"`
-        	Issuer string `json:"issuer"`
-        	Reason string `json:"reason"`
-        }
-        
-        resp, _ := http.PostForm("https://browserid.org/verify", url.Values{"assertion": {token}, "audience": {Hostname}})
+		var token = r.URL.Query().Get("token")
+
+		type browserIdVerificationResponse struct {
+			Status   string `json:"status"`
+			Email    string `json:"email"`
+			Audience string `json:"audience"`
+			Expires  int64  `json:"expiers"`
+			Issuer   string `json:"issuer"`
+			Reason   string `json:"reason"`
+		}
+
+		resp, _ := http.PostForm("https://browserid.org/verify", url.Values{"assertion": {token}, "audience": {Hostname}})
 		defer resp.Body.Close()
 		resultJson, _ := ioutil.ReadAll(resp.Body)
-		
-		fmt.Printf("%s\n",resultJson)
-        
-        var result browserIdVerificationResponse
-        err := json.Unmarshal(resultJson, &result)
-        
-        if err != nil {
-        	fmt.Println("error",err)
-        }
-        
-        fmt.Println(result)
-        
-        var data []byte
-        
-        if result.Status == "okay" {
-        	fmt.Println("okay")
-            user := yack.GetModel().Users.GetByEmail(result.Email)
-            if user == nil {
-                yack.NewUser(result.Email)
-                user = yack.GetModel().Users.GetByEmail(result.Email)
-            }
-             
-            var m = loginSucessMessage{true, user.Id(), user.DisplayName(), user.AuthToken()}
-		
+
+		fmt.Printf("%s\n", resultJson)
+
+		var result browserIdVerificationResponse
+		err := json.Unmarshal(resultJson, &result)
+
+		if err != nil {
+			fmt.Println("error", err)
+		}
+
+		fmt.Println(result)
+
+		var data []byte
+
+		if result.Status == "okay" {
+			fmt.Println("okay")
+			user := yack.GetModel().Users.GetByEmail(result.Email)
+			if user == nil {
+				yack.NewUser(result.Email)
+				user = yack.GetModel().Users.GetByEmail(result.Email)
+			}
+
+			var m = loginSucessMessage{true, user.Id(), user.DisplayName(), user.AuthToken()}
+
 			data, _ = json.Marshal(m)
-			fmt.Println("generated json: ",string(data))
-                
-        } else {
-        
-        	var m = loginFailMessage{false, fmt.Sprintf("BrowserId status is '%s' ('okay' excepted), reason: %s", result.Status, result.Reason)}
-        	data, _ = json.Marshal(m)
-        }
-        w.Header().Set("Content-Type", "application/javascript")
+			fmt.Println("generated json: ", string(data))
+
+		} else {
+
+			var m = loginFailMessage{false, fmt.Sprintf("BrowserId status is '%s' ('okay' excepted), reason: %s", result.Status, result.Reason)}
+			data, _ = json.Marshal(m)
+		}
+		w.Header().Set("Content-Type", "application/javascript")
 		w.WriteHeader(200)
 		w.Write(data)
-	
+
 	} else {
 		w.WriteHeader(404)
 	}

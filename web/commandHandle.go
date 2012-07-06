d0630727786ebc9aba1e88a68fe16442797ed1f2
Frédéric Bertolus <fred.bertolus@gmail.com>
2012-07-06 23:57:12 +0200

    begin to add file list

--- web/commandHandle.go
+++ web/commandHandle.go
@@ -19,24 +19,12 @@ func NewCommandHandle() *CommandHandle {
 	return &this
 }
 
-/*type loginSucessMessage struct {
-	Status bool  `json:"status"`
-	Id int  `json:"id"`
-	Name string  `json:"name"`
-	Token string  `json:"token"`
-}
-
-
-type loginFailMessage struct {
-	Status bool `json:"status"`
-	Error string `json:"error"`
-}*/
-
 type fileMessage struct {
 	Id       int    `json:"id"`
 	Name     string `json:"name"`
 	Size     int    `json:"size"`
 	Link     string `json:"link"`
+	Progress float64`json: progress`
 	CanWrite bool   `json:"can_write"`
 }
 
@@ -82,106 +70,45 @@ func (this CommandHandle) ServeHTTP(
 			return
 		}
 
-		var files []*yack.File = user.GetInterruptedFiles()
-		var fileMessages []fileMessage = make([]fileMessage, len(files))
+		writeFileList(w, user.GetInterruptedFiles(), user)
+	} else if cmd == "getFileList" {
+    	var path = r.URL.Query().Get("path")
+    	if path == "" && user != nil {
+        	fmt.Println("CommandHandle: user id=",user.Id()," str=",strconv.Itoa(user.Id()) )
+    	    path = strconv.Itoa(user.Id())
+    	}
+    	
+    	var pack *yack.Pack = yack.GetModel().Packs.GetByPath(path)
+	
+	    if pack == nil {
+	        writeError(w, "No pack found at this path: "+path)
+	        return
+	    }
+	    
+	    if !pack.CanRead(user) {
+	        writeError(w, "You don't have the right to read the pack: "+path)
+	        return
+	    }
+	    
+		writeFileList(w, pack.GetFiles(), user)
+	} else {
+    	writeError(w, "Unknown command: "+cmd)
+	}
+	
+}
+
+func writeFileList(w http.ResponseWriter, files []*yack.File, user *yack.User) {
+var fileMessages []fileMessage = make([]fileMessage, len(files))
 
 		for i, file := range files {
-			var link string = "/file?id=" + string(file.Id()) + "&sha=" + file.Sha()
-			fileMessages[i] = fileMessage{file.Id(), file.Name(), file.Size(), link, file.CanWrite(user)}
+			var link string = "/file?id=" + strconv.Itoa(file.Id()) + "&sha=" + file.Sha()
+			fileMessages[i] = fileMessage{file.Id(), file.Name(), file.Size(), link, file.Progress(), file.CanWrite(user)}
 		}
 
 		var m = fileListMessage{fileMessages}
 		var data []byte
 		data, _ = json.Marshal(m)
 		writeResponse(w, data)
-	}
-
-	/*   
-
-	   files = YackFile.objects.all()
-	   data = json.dumps([{'id': yackFile.pk,'size': yackFile.size, 'progress': yackFile.get_progress(), 'name': yackFile.name , 'link': "/file?pk="+str(yackFile.pk)+"&sha="+str(yackFile.sha), 'can_write': yackFile.can_write(auth_user) }  for yackFile in files  if yackFile.can_write(auth_user)  and yackFile.get_progress() < 1])
-	   return HttpResponse(data,mimetype)
-
-	*/
-
-	/*
-			if method == "check" {
-				var token = r.URL.Query().Get("token")
-				var id , _= strconv.Atoi(r.URL.Query().Get("id"))
-
-				// database.users.get(auth_token=token, id=id)
-				var user = model.GetModel().Users.GetByAuthToken(token, id)
-		        var data []byte
-
-		        if user == nil {
-		            var m = loginFailMessage{false, fmt.Sprintf("AuthToken is invalid or perimed: %s", token)}
-		        	data, _ = json.Marshal(m)
-		        } else {
-				    var m = loginSucessMessage{true, user.Id(), user.DisplayName(), user.AuthToken()}
-				    data, _ = json.Marshal(m)
-				}
-				fmt.Println("generated verify json: ",string(data))
-				w.Header().Set("Content-Type", "application/javascript")
-				w.WriteHeader(200)
-				w.Write(data)
-
-		        //except ObjectDoesNotExist:
-		        //    data = json.dumps([{'status':  False}])
-
-			} else if method == "browserid" {
-			 	var token = r.URL.Query().Get("token")
-
-		        type browserIdVerificationResponse struct {
-		        	Status string `json:"status"`
-		        	Email string `json:"email"`
-		        	Audience string `json:"audience"`
-		        	Expires int64 `json:"expiers"`
-		        	Issuer string `json:"issuer"`
-		        	Reason string `json:"reason"`
-		        }
-
-		        resp, _ := http.PostForm("https://browserid.org/verify", url.Values{"assertion": {token}, "audience": {yack.Hostname}})
-				defer resp.Body.Close()
-				resultJson, _ := ioutil.ReadAll(resp.Body)
-
-				fmt.Printf("%s\n",resultJson)
-
-		        var result browserIdVerificationResponse
-		        err := json.Unmarshal(resultJson, &result)
-
-		        if err != nil {
-		        	fmt.Println("error",err)
-		        }
-
-		        fmt.Println(result)
-
-		        var data []byte
-
-		        if result.Status == "okay" {
-		        	fmt.Println("okay")
-		            user := model.GetModel().Users.GetByEmail(result.Email)
-		            if user == nil {
-		                model.NewUser(result.Email)
-		                user = model.GetModel().Users.GetByEmail(result.Email)
-		            }
-
-		            var m = loginSucessMessage{true, user.Id(), user.DisplayName(), user.AuthToken()}
-
-					data, _ = json.Marshal(m)
-					fmt.Println("generated json: ",string(data))
-
-		        } else {
-
-		        	var m = loginFailMessage{false, fmt.Sprintf("BrowserId status is '%s' ('okay' excepted), reason: %s", result.Status, result.Reason)}
-		        	data, _ = json.Marshal(m)
-		        }
-		        w.Header().Set("Content-Type", "application/javascript")
-				w.WriteHeader(200)
-				w.Write(data)
-
-			} else {
-				w.WriteHeader(404)
-			}*/
 }
 
 func writeResponse(w http.ResponseWriter, data []byte) {
